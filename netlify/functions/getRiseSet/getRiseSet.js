eb7aade5ee50ad01f45e75f8f607bd308759c79f
Jo Blannin <jbla0017@mu00156342x.modem>
2025-04-10 11:05:49 +1000

    Update environment variable names to match Netlify format

--- netlify/functions/getRiseSet/getRiseSet.js
+++ netlify/functions/getRiseSet/getRiseSet.js
@@ -35,7 +35,7 @@ exports.handler = async function(event, context) {
         bodies: ['moon', 'jupiter', 'saturn', 'venus', 'mars']
       },
       headers: {
-        'Authorization': `Basic ${Buffer.from(`${process.env.ASTRONOMY_API_ID}:${process.env.ASTRONOMY_API_SECRET}`).toString('base64')}`
+        'Authorization': `Basic ${Buffer.from(`${process.env.ASTRONOMYAPIID}:${process.env.ASTRONOMYAPISECRET}`).toString('base64')}`
       }
     });
 
